01ac0016022fd1913dd0f490df8a9a1f37219ad5
Victor Mazzola <victormazzola2019@gmail.com>
2025-01-18 19:17:40 -0300

    initial commit react, vite, tailwind, axios

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div className="w-full h-full relative">
+            <div className="text-center p-4">
+                <input type="text" className="py-3 px-6 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400 focus:outline-none bg-white-600/100 shadow-md"/>
+            </div>
+        </div>
+
+    )
+}
+
+export default App
