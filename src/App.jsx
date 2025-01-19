87bbf2977bd4711d23686550cd6da5991ffc53de
Victor Mazzola <victormazzola2019@gmail.com>
2025-01-19 11:21:19 -0300

    add weatherData

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
     const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
-    const url = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${API_KEY}`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`;
 
     const searchLocation = async (event) => {
         if (event.key === "Enter") {
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -6,12 +6,27 @@ const Weather = ({weatherData}) => {
                 shadow-lg rounded-xl m-auto relative px-6 top-[10%]
                 '>
                     <div className="flex justify-between w-full">
-
+                        <div className="w-1/2 my-4 mx-auto flex justify-between items-center">
+                            <div className="flex flex-col items-start justify-between h-full">
+                                <div>
+                                    <p className='text-xl'>
+                                        {weatherData.name},
+                                        {weatherData.sys.country}
+                                    </p>
+                                    <p className='text-sm'>
+                                        {weatherData.weather[0].description}
+                                    </p>
+                                </div>
+                                <div>
+                                    <h1 className="text-6xl font-semibold"> {weatherData.main.temp.toFixed()} ºC</h1>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             ) : null}
         </div>
-    )
-}
+    );
+};
 
 export default Weather;
