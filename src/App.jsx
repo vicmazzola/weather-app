3fa64140754ca5b969b5bea209844571f122e217
Victor Mazzola <victormazzola2019@gmail.com>
2025-01-19 08:43:48 -0300

    add searchLocation and fix .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import './App.css'
 import {useState} from "react";
+import axios from "axios";
 
 function App() {
 
-    const [data,setData] = useState({})
+    const [data, setData] = useState({})
     const [location, setLocation] = useState("")
 
     const API_KEY = process.env.REACT_APP_API_KEY;
-    const url = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API_KEY}`
+    const url = `http://api.openweathermap.org/data/2.5/forecast?id=${location}&appid=${API_KEY}`;
+
+    const searchLocation = (event) => {
+        if(event.key === "Enter") {
+            axios.get(url)
+                .then((response) => {
+                    setData(response.data)
+                    console.log(response.data)
+                })
+            setLocation("")
+        }
+    }
 
     return (
         <div className="w-full h-full relative">
@@ -18,7 +30,13 @@ function App() {
                        text-lg rounded-3xl border border-gray-200 text-gray-600
                        placeholder:text-gray-400 focus:outline-none
                        bg-white-600/100 shadow-md"
-                       placeholder="Enter location"/>
+                       placeholder="Enter location"
+                       value={location}
+                       onChange={(e) => {
+                           setLocation(e.target.value)
+                       }}
+                       onKeyDownCapture={searchLocation}
+                />
             </div>
         </div>
 
