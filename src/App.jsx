8f1e8c0a9bd64989ebffb2d2807ecfa62abfcdf3
Victor Mazzola <victormazzola2019@gmail.com>
2025-01-19 08:30:35 -0300

    add .env REACT_APP_API_KEY

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
+import {useState} from "react";
 
 function App() {
+
+    const [data,setData] = useState({})
+    const [location, setLocation] = useState("")
+
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    const url = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API_KEY}`
 
     return (
         <div className="w-full h-full relative">
             <div className="text-center p-4">
-                <input type="text" className="py-3 px-6 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400 focus:outline-none bg-white-600/100 shadow-md"/>
+                <input type="text"
+                       className="
+                       py-3 px-6 w-[700px]
+                       text-lg rounded-3xl border border-gray-200 text-gray-600
+                       placeholder:text-gray-400 focus:outline-none
+                       bg-white-600/100 shadow-md"
+                       placeholder="Enter location"/>
             </div>
         </div>
 
