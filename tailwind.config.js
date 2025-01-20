0eb087ba86b0e74654e4c4e3014a664b600e3ec6
Victor Mazzola <victormazzola2019@gmail.com>
2025-01-20 08:00:31 -0300

    delete app.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: '0 4px 6px var(--tw-shadow-color, rgba(0, 0, 0, 0.1))',
+      },
+    },
   },
   plugins: [],
 };
